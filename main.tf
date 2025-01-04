0a786d335bd0d97ecd9a3ccda3b627d8437d6880
Sunny Nwosu <sunnyokenwosu@gmail.com>
2025-01-04 14:50:53 +0000

    Deploying a cluster of web servers: ASG

--- main.tf
+++ main.tf
@@ -18,6 +18,48 @@
     }
  }
 
+# Create Launch Config
+ resource "aws_launch_configuration" "prod"{
+   image_id = "ami-05c172c7f0d3aed00"
+   instance_type = "t2.micro"
+   security_groups = [ aws_security_group.instance.id ]
+   user_data = <<-EOF
+               #!bin/bash
+               echo "Hello, world" > index.html
+               nohup busybox httpd -f -p ${var.server_port} &
+               EOF
+
+# Required when using a launch configuration with an auto scaling group.
+   lifecycle {
+     create_before_destroy = true
+   }
+ }
+
+# Use a Data Source to fetch the default VPC
+data "aws_vpc" "default" {
+   default = true 
+}
+
+# Usa a Data Source to lookup subnets with the vpc
+data "aws_subnets" "default"{
+   filter {
+     name = "vcp-id"
+     values = [ data.aws_vpc.default.id ]
+   } 
+}
+# Create Auto scaling Group 
+resource "aws_autoscaling_group" "prod" {
+   launch_configuration = aws_launch_configuration.prod
+   vpc_zone_identifier = data.aws_subnets.default.ids # Dynamically fetched subnets
+   min_size = 2
+   max_size = 6
+
+   tag {
+     key = "Name"
+     value = "ASGInstance"
+     propagate_at_launch = true
+   }
+}
  resource "aws_security_group" "instance"{
    name = "caly-prod-instance"
 
