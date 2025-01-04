3cac2180af1ab1be9245c1cc79c0c2b36eb63b8f
Sunny Nwosu <sunnyokenwosu@gmail.com>
2025-01-04 14:48:04 +0000

    Deploying a cluster of web servers: ASG

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@
     user_data = <<-EOF
                #!bin/bash
                echo "Hello, world" > index.html
-               nohup busybox httpd -f -p 8080 &
+               nohup busybox httpd -f -p ${var.server_port} &
                EOF
       user_data_replace_on_change = true
 
@@ -22,8 +22,8 @@
    name = "caly-prod-instance"
 
    ingress {
-      from_port = 8080
-      to_port = 8080
+      from_port = var.server_port
+      to_port = var.server_port
       protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
    }
